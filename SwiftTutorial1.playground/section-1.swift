a00a86150c533a54190e9795c2f488bdd7596e00
55011221005Pramot <doosee.den@gmail.com>
2014-08-22 00:06:31 +0700

    Update section-1.swift

--- SwiftTutorial1.playground/section-1.swift
+++ SwiftTutorial1.playground/section-1.swift
@@ -21,3 +21,147 @@ if (onSaleInferred) {
 } else {
     println("\(nameInferred) at regular price: \(priceInferred)!")
 }
+
+var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Eggs","Milk"]
+//var   shoppingListExplicit: [String] = [Eggs","Milk"]
+
+var shoppingList = ["Eggs","Milk"]
+println("The shopping list contins \(shoppingList.count) items.")
+shoppingList.append("Flour")
+shoppingList += "Baking Powder"
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+var firstItem = shoppingLisft[0]
+shoppingList[0] = "Six eggs"
+//shoppingList[0...3] = ["Bananas", "Apples"]
+shoppingList
+
+let posssibleTipsInferred = [0.15,0.18,0.20]
+let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+var numberOfItems = possibleTipsInferred.count
+
+let apples = shoppingList.removeLast()
+//the last item the array has just been removed
+//shoppingList now contains 5 items, and no apples
+//the apples constat is now equal to the removed "Apples" string
+
+let mapleSyrup = shoppingList.removeAtIndex(0)
+//the item that was at index 0 has just been removed
+//shoppingList now contains 6 items, and no Maple Syrup
+//the mapleSyrup contant is now equal to the removed "Maple Syrup" string
+
+
+let array: Array<Int> = [1,2,3,4]
+let dictionary: Dictionary<String, Int> = ["dog":1, "elephent": 2]
+
+
+var airports:[String: String] = ["TYO": "Tokyo", "DUB": "Dublin"]
+
+if  airports.isEmpty{
+    println("The airports dictionary is empty.")
+} else {
+    println("The airports dictionary is not empty.")
+}
+// prints "the airports dictionary is not empty."
+
+println(The airports dictionary contains \(airports.count) items.")
+//  prints "The airports dictionary contains 2 items."
+
+airports["LHR"] = "London"
+// the airports dictionary now contains 3 items
+
+
+
+airports["LHR"] = "London Heathrow"
+//the value for "LHR" has been changed to "London Heathow"
+
+
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+    println("The old value for DUB was \(oldValue).")
+}
+// prints "The old value for DUB was Dublin."
+
+
+airports[APL"] = "Apple International"
+//
+airports["APL"] = nil
+//
+
+if  let removedValue = airports.removeValueForKey("DUB") {
+    println("The removed airport's name is \(removedValue).")
+} else {
+    println("The airports dictionary dose not contain a value for DUB.")
+}
+//
+
+
+
+
+
+
+
+class   TipCalculator{
+
+    //2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    //3
+    init(total:Double, taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    //4
+    func    calcTipWithTipPct(tipPct:Double) -> Double{
+            return  subtotal * tipPct
+    }
+    
+    //1
+    func    returnPossibleTips() -> [Int: Double]{
+    
+            let possibleTipsInferred = [0.15,0.18,0.20]
+            let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+            var numberOfItems = possibleTipsInferred.count
+            
+            //2
+            var ratval = [Int: Double]()
+            for possibleTip in possibleTipsInferred{
+                let intPct = Int(possibleTip*100)
+                //3
+                retval[intPct] = calcTipWithTipPct(possibleTip)
+            }
+            return retval
+        }
+    }
+    
+    //6
+    let tipCalc = TipCalculator(total:33.25, taxPct: 0.06)
+    tipCalc.returnPossibleTips()
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
