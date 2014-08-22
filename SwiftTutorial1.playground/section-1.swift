85c9b150320c20bfa1671db71f651ef975b8a32a
55011221005Pramot <imac@MSU>
2014-08-22 14:57:12 +0700

    เพิ่ม code remove dictionary

--- SwiftTutorial1.playground/section-1.swift
+++ SwiftTutorial1.playground/section-1.swift
@@ -33,7 +33,7 @@ shoppingList += "Baking Powder"
 shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
 var firstItem = shoppingList[0]
 shoppingList[0] = "Six eggs"
-//shoppingList[0...3] = ["Bananas", "Apples"]
+shoppingList[0...3] = ["Bananas", "Apples"]
 shoppingList
 
 if(onSaleInferred){
@@ -43,14 +43,66 @@ if(onSaleInferred){
     println("\(nameInferred) at regular ptice: \(priceInferred)!")
 }
 
+shoppingList += ["Baking Powder"]
+//shoppingList now contain 4 items
+shoppingList += ["Chocolate Spread","Cheese"]
+shoppingList.insert("Maple Syrup", atIndex: 0 )
+shoppingList.append("Flour")
+shoppingList += ["Baking Powder"]
+
+var firstItem2 = shoppingList[0]
+shoppingList[4...6] = ["Bananas","Apples"]
+
+let apples = shoppingList.removeLast()
+//the last item the array has just been removed
+//shoppingList now contains 5 items, and no apples
+//the apples constat is now equal to the removed "Apples" string
+
+let mapleSyrup = shoppingList.removeAtIndex(0)
+//the item that was at index 0 has just been removed
+//shoppingList now contains 6 items, and no Maple Syrup
+//the mapleSyrup contant is now equal to the removed "Maple Syrup" string
+
+let array: Array<Int> = [1,2,3,4]
+let dictionary: Dictionary<String, Int> = ["dog":1, "elephent": 2]
+
+
+var airports:[String: String] = ["TYO": "Tokyo", "DUB": "Dublin"]
+
+if  airports.isEmpty{
+    println("The airports dictionary is empty.")
+} else {
+    println("The airports dictionary is not empty.")
+}
+// prints "the airports dictionary is not empty."
+
+println("The airports dictionary contains \(airports.count) items.")
+//  prints "The airports dictionary contains 2 items."
+
+
+airports["LHR"] = "London"
+// the airports dictionary now contains 3 items
 
 
 
+airports["LHR"] = "London Heathrow"
+//the value for "LHR" has been changed to "London Heathow"
 
 
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+    println("The old value for DUB was \(oldValue).")
+}
+// prints "The old value for DUB was Dublin."
 
 
-
-
-
-
+airports["APL"] = "Apple International"
+    //
+    airports["APL"] = nil
+    //
+    
+if  let removedValue = airports.removeValueForKey("DUB") {
+    println("The removed airport's name is \(removedValue).")
+} else {
+    println("The airports dictionary dose not contain a value for DUB.")
+}
+//
