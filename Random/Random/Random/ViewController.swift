525c8927f0339477c1226820b033f2d193c4a137
55011212018ark <imac@msu>
2014-09-24 16:17:38 +0700

    แบบฝึก random

--- Random/Random/Random/ViewController.swift
+++ Random/Random/Random/ViewController.swift
@@ -9,6 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var i=0,j=0,k=0
+ 
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var three: UIButton!
+    
+    
+    @IBOutlet weak var oneL: UILabel!
+    @IBOutlet weak var twoL: UILabel!
+    @IBOutlet weak var treeL: UILabel!
+    
+    @IBOutlet weak var reset: UIButton!
+    
+    
+    @IBAction func one(sender: UIButton) {
+        i=i+1
+        oneL.text = String(i)
+    }
+    @IBAction func two(sender: UIButton) {
+        j=j+1
+        twoL.text = String(j)
+    }
+    @IBAction func threee(sender: UIButton) {
+        k=k+1
+        treeL.text = String(k)
+    }
+   
+    @IBAction func resetFunc(sender: UIButton) {
+        i=0
+        j=0
+        k=0
+        oneL.text = String(i)
+        twoL.text = String(j)
+        treeL.text = String(k)
+        
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
