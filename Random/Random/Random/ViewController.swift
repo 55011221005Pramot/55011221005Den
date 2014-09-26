8274fca21260f096abc2541a9cd176d6551fa458
55011212018ark <imac@msu>
2014-09-26 15:14:09 +0700

    แก้ไข code random

--- Random/Random/Random/ViewController.swift
+++ Random/Random/Random/ViewController.swift
@@ -11,42 +11,49 @@ import UIKit
 class ViewController: UIViewController {
     
     var i=0,j=0,k=0
- 
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var twoButton: UIButton!
-    @IBOutlet weak var three: UIButton!
-    
-    
+    @IBOutlet weak var oneTF: UIButton!
     @IBOutlet weak var oneL: UILabel!
+    
+    @IBOutlet weak var twoTF: UIButton!
     @IBOutlet weak var twoL: UILabel!
-    @IBOutlet weak var treeL: UILabel!
+    
+    
+    @IBOutlet weak var threeTF: UIButton!
+    @IBOutlet weak var threeL: UILabel!
     
     @IBOutlet weak var reset: UIButton!
     
     
-    @IBAction func one(sender: UIButton) {
-        i=i+1
+    @IBAction func add1(sender: AnyObject) {
+        i++
         oneL.text = String(i)
+        
     }
-    @IBAction func two(sender: UIButton) {
-        j=j+1
+    
+    
+    @IBAction func add2(sender: AnyObject) {
+        j++
         twoL.text = String(j)
     }
-    @IBAction func threee(sender: UIButton) {
-        k=k+1
-        treeL.text = String(k)
+    
+    
+    @IBAction func add3(sender: AnyObject) {
+        k++
+        threeL.text = String(k)
+        
     }
-   
-    @IBAction func resetFunc(sender: UIButton) {
+    
+    
+    
+    
+    @IBAction func resetValue(sender: AnyObject) {
         i=0
         j=0
         k=0
         oneL.text = String(i)
         twoL.text = String(j)
-        treeL.text = String(k)
-        
+        threeL.text = String(k)
     }
-    
 
 
     override func viewDidLoad() {
