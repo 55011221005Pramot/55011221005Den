1263a00121e4598146e594d46d9a7d7ad5811f57
55011212181Biiw <iMacATMSU>
2014-10-10 16:05:08 +0700

    mid

---
+++ Exam1_55011221005/Exam1_55011221005/calculator.swift
@@ -0,0 +1,23 @@
+//
+//  calculator.swift
+//  Exam1_55011221005
+//
+//  Created by Student on 10/10/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import Foundation
+class calculator {
+    
+    var volume:Double
+    var price:Double
+    var sum:Double{
+    get{  return    }
+    }
+    
+    init(volume:Double,price:Double){
+        self.volume = volume
+        self.price = price
+    }
+    
+}
